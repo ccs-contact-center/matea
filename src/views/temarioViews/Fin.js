7105f1709f7df877f0642e2350e6d601ca72a4fe
DanielDO93 <danieldionisio1993@gmail.com>
2020-11-11 14:05:19 -0600

    Update Fin.js

--- src/views/temarioViews/Fin.js
+++ src/views/temarioViews/Fin.js
@@ -14,29 +14,7 @@ class Fin extends Component {
         </CardHeader>
         <CardBody className="">
           <Row className="centrado-fila">
-            <Col xs="12">
-              <div className="cajaGris">
-                <h3>
-                  GRACIAS
-                </h3>
-                <p>
-                  Tratar las objeciones requiere práctica. Te invitamos a que pongas en practica lo aprendido
-                  hoy para que mejores en el desempeño de tus llamadas.
-              </p>
-                <p>
-                  Peter F. Drucker dijo:<br/>
-                  “La calidad de un producto o servicio no es lo que has puesto en él, es lo que el cliente 
-                  obtiene de él”.
-
-              </p>
-                <p>
-
-                  Se trata de utilizar lo averiguado en la investigación y vender tu producto desde el 
-                  beneficio para el cliente.
-
-              </p>
-              </div>
-            </Col>
+           
             <Col xs="9">
               <Row className="centrado-fila mt-3">
                 <Col xs="12">
@@ -45,7 +23,7 @@ class Fin extends Component {
                     className="cajaRoja
                    ml-2 mr-2"
                   >
-                    {/* <h2 className="text-center">Gracias</h2> */}
+                    <h2 className="text-center">Gracias</h2> 
                     <p className="text-center">
                       www.ccscontactcenter.com
                       <br />
